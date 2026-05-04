cec1f1509d13e4886f5004bc75c1396e352bba6d
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2026-05-04 09:11:44 +0700

    fix: added model for comparasion of inference

--- paper_evalute/inference_speed_test.py
+++ paper_evalute/inference_speed_test.py
@@ -54,115 +54,31 @@ class SE2_CNNET(nn.Module):
         x  = self.up3(x, x2);  x = self.up4(x, x1)
         return self.outc(x).tensor
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class FCN_VGG(nn.Module):
+    def __init__(self, vgg_model, n_classes=2):
         super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True)
+        self.features = vgg_model.features
+        self.classifier = nn.Sequential(
+            nn.Conv2d(512, 4096, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(),
+            nn.Conv2d(4096, n_classes, kernel_size=1)
         )
-    def forward(self, x): return self.double_conv(x)
-
-class StandardUNet(nn.Module):
-    def __init__(self, n_channels=3, n_classes=2):
-        super(StandardUNet, self).__init__()
-        self.inc    = DoubleConv(n_channels, 64)
-        self.down1  = nn.Sequential(nn.MaxPool2d(2), DoubleConv(64, 128))
-        self.down2  = nn.Sequential(nn.MaxPool2d(2), DoubleConv(128, 256))
-        self.down3  = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512))
-        self.up1    = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.conv1  = DoubleConv(512, 256)
-        self.up2    = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.conv2  = DoubleConv(256, 128)
-        self.up3    = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.conv3  = DoubleConv(128, 64)
-        self.outc   = nn.Conv2d(64, n_classes, kernel_size=1)
-
     def forward(self, x):
-        x1 = self.inc(x);  x2 = self.down1(x1)
-        x3 = self.down2(x2); x4 = self.down3(x3)
-        x  = self.up1(x4); x = torch.cat([x, x3], dim=1); x = self.conv1(x)
-        x  = self.up2(x);  x = torch.cat([x, x2], dim=1); x = self.conv2(x)
-        x  = self.up3(x);  x = torch.cat([x, x1], dim=1); x = self.conv3(x)
-        return self.outc(x)
+        x = self.features(x)
+        x = self.classifier(x)
+        return F.interpolate(x, size=(256, 256), mode='bilinear', align_corners=False)
 
-class AttentionBlock(nn.Module):
-    def __init__(self, F_g, F_l, F_int):
-        super(AttentionBlock, self).__init__()
-        self.W_g = nn.Sequential(
-            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-        self.W_x = nn.Sequential(
-            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-        self.psi = nn.Sequential(
-            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
-        )
-        self.relu = nn.ReLU(inplace=True)
+def get_vgg16_fcn():
+    import torchvision.models as models
+    return FCN_VGG(models.vgg16(weights=None), 2)
 
-    def forward(self, g, x):
-        g1 = self.W_g(g)
-        x1 = self.W_x(x)
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-        return x * psi
-
-class AttentionUNet(nn.Module):
-    def __init__(self, n_channels=3, n_classes=2):
-        super(AttentionUNet, self).__init__()
-        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.Conv1 = DoubleConv(n_channels, 64)
-        self.Conv2 = DoubleConv(64, 128)
-        self.Conv3 = DoubleConv(128, 256)
-        self.Conv4 = DoubleConv(256, 512)
-
-        self.Up4 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.Att4 = AttentionBlock(F_g=256, F_l=256, F_int=128)
-        self.Up_conv4 = DoubleConv(512, 256)
-
-        self.Up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.Att3 = AttentionBlock(F_g=128, F_l=128, F_int=64)
-        self.Up_conv3 = DoubleConv(256, 128)
-
-        self.Up2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.Att2 = AttentionBlock(F_g=64, F_l=64, F_int=32)
-        self.Up_conv2 = DoubleConv(128, 64)
-
-        self.Conv_1x1 = nn.Conv2d(64, n_classes, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        e1 = self.Conv1(x)
-        e2 = self.Maxpool(e1)
-        e2 = self.Conv2(e2)
-        e3 = self.Maxpool(e2)
-        e3 = self.Conv3(e3)
-        e4 = self.Maxpool(e3)
-        e4 = self.Conv4(e4)
-
-        d4 = self.Up4(e4)
-        x3 = self.Att4(g=d4, x=e3)
-        d4 = torch.cat((x3, d4), dim=1)
-        d4 = self.Up_conv4(d4)
-
-        d3 = self.Up3(d4)
-        x2 = self.Att3(g=d3, x=e2)
-        d3 = torch.cat((x2, d3), dim=1)
-        d3 = self.Up_conv3(d3)
-
-        d2 = self.Up2(d3)
-        x1 = self.Att2(g=d2, x=e1)
-        d2 = torch.cat((x1, d2), dim=1)
-        d2 = self.Up_conv2(d2)
-
-        out = self.Conv_1x1(d2)
-        return out
+def get_vgg19_fcn():
+    import torchvision.models as models
+    return FCN_VGG(models.vgg19(weights=None), 2)
 
 import torchvision.models.segmentation as seg_models
 
@@ -282,8 +198,8 @@ def run_benchmark():
     
     models = {
         "Mod-Seg-SE(2)": SE2_CNNET(n_channels=3, n_classes=2, N=8, base_channels=24).to(device),
-        "Standard U-Net": StandardUNet(n_channels=3, n_classes=2).to(device),
-        "Attention U-Net": AttentionUNet(n_channels=3, n_classes=2).to(device),
+        "VGG16 (FCN)": get_vgg16_fcn().to(device),
+        "VGG19 (FCN)": get_vgg19_fcn().to(device),
         "ResNet50 (DeepLabV3)": DeepLabV3_ResNet50().to(device),
         "ResNet101 (FCN)": FCN_ResNet101().to(device)
     }
